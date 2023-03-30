7e1ffa71e3be8bac1bc3bce202bfe161a69effd6
barakisbrown <lokislayer@gmail.com>
2023-03-29 22:17:47 -0500

    First Commit plus creation of the BST-Data which will contaiin my model and DbContext.

---
+++ BloodSugarTracker/BST-Data/BSTContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BST_Data
+{
+    public class BSTContext : DbContext
+    {
+        public DbSet<BloodSugar> Readings { get; set; }
+
+        public string DbPath { get; set; }
+
+        public BSTContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "readings.db");
+        }
+
+        // CREATES THE SQL DB
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite($"Data Source = {DbPath}");
+    }
+}
---
+++ BloodSugarTracker/BST-Data/BloodSugar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_Data
+{
+    /// <summary>
+    /// BloodSugar represents one unit of entry for my application.  The tracker will consist of this 
+    /// class plus some analytics of this said class.
+    /// </summary>
+    public class BloodSugar
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        public TimeOnly? TimeMessured { get; set; }
+        [Required]
+        public DateOnly DateMessured { get; set; }
+        public String? Notes { get; set; }
+    }
+}
